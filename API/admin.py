0318ff72e40bd6b2995d7ecf630a996fe2a82f8a
Upendra Upadhyay <upendra.upadhyay.97@gmail.com>
2018-05-25 11:15:10 +0530

    Created a model UpdateStatus.

--- API/admin.py
+++ API/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from API.models import UpdateStatus
 
 # Register your models here.
+admin.site.register(UpdateStatus)
--- API/models.py
+++ API/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from users.models import InoDriver
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+
+
+class UpdateStatus(models.Model):
+    bus_number = models.CharField(max_length=100)
+    previous_driver_name = models.CharField(_('Previous Driver Name'), max_length=130, blank=True)
+    new_driver_name = models.CharField(_('New Driver Name'), max_length=130, blank=True)
+    phone_number = models.BigIntegerField(null=True, blank=True)
+    departure_time = models.DateTimeField()
