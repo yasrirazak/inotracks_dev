01ff1e7613c3e6a4c873b44fc4f25a719cb32a63
kunal <kunalgk1999@gmail.com>
2018-05-26 23:56:03 +0530

    new req geopy for new script speed to calculate speed

---
+++ helper/speed.py
@@ -0,0 +1,26 @@
+from geopy.distance import geodesic
+import datetime
+
+
+def get_distance_in_km(lprev, lnew):
+    # l = (lat, lon) tuple
+
+    distace = geodesic(lprev, lnew)
+    distace_in_km = distace.km
+
+    return distace_in_km
+
+
+def get_speed_in_kmph(lprev, lnew, tprev, tnew):
+    # l = (lat, lon) tuple of lattitude and logitude
+    # t = datetime.datetime object
+
+    distance_in_km = get_distance_in_km(lprev, lnew)
+
+    diff = tnew - tprev
+    diff_in_sec = diff.seconds
+    diff_in_hr = diff_in_sec / 3600
+
+    speed_in_kmph = distance_in_km / diff_in_hr
+
+    return speed_in_kmph
